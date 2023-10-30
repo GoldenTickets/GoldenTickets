6e22bf48e5130bb7d6151274939303ec1c799a71
YMJ936 <aud780@gmail.com>
2023-10-30 21:17:28 +0900

    make SwaggerConfig again

--- src/main/java/com/goldentickets/controller/MovieApiController.java
+++ src/main/java/com/goldentickets/controller/MovieApiController.java
@@ -1,8 +1,8 @@
 package com.goldentickets.controller;
 
-import com.goldentickets.domain.Review;
-import com.goldentickets.dto.ReviewResponse;
-import com.goldentickets.service.ReviewService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import com.goldentickets.domain.Movie;
 import com.goldentickets.dto.MovieResponse;
@@ -15,14 +15,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
 @RequiredArgsConstructor
 @Controller
+@Tag(name = "moiveInfo", description = "MI API document")
 public class MovieApiController {
 
     private final MovieService movieService;
 
     @GetMapping("/api/movies/{m_seq}")
+    @Operation(summary = "load", description = "to load MV", tags = {"m_seq"})
     public String findMovies(@PathVariable long m_seq, Model model) {
 
         Movie movie = movieService.findById(m_seq);
