ba63ce145a71066cd42e096f455a6123b64fa409
YMJ936 <aud780@gmail.com>
2023-11-17 17:11:58 +0900

    make member files

--- src/main/resources/static/js/signup.js
+++ src/main/resources/static/js/signup.js
@@ -1,9 +1,9 @@
-// 회원가입
+// 회원가입 미완
 const signupButton = document.getElementById('signup-btn');
 
 if (signupButton) {
     signupButton.addEventListener('click', event => {
-        fetch('/api/signup', {
+        fetch('/member', {
             method: 'POST',
             headers: {
                 "Content-Type": "application/json",
@@ -22,5 +22,28 @@ if (signupButton) {
                 alert('회원가입이 완료되었습니다.');
                 location.replace('loginpage');
             });
+    });
+}
+
+// 로그인
+const loginButton = document.getElementById('login-btn');
+
+if (loginButton) {
+    // 등록 버튼을 클릭하면 /api/articles로 요청을 보낸다
+    loginButton.addEventListener('click', event => {
+        body = JSON.stringify({
+            email: document.getElementById('email').value,
+            password: document.getElementById('password').value
+        });
+        function success() {
+            alert('로그인에 성공했습니다.');
+            location.replace('/board');
+        };
+        function fail() {
+            alert('로그인에 실패했습니다.');
+            location.replace('/board');
+        };
+
+        httpRequest('GET','/member', body, success, fail)
     });
 }
