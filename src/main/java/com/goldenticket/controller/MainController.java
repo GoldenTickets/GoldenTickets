87207dcf5d491db14254b52163c520d4cc593906
YMJ936 <aud780@gmail.com>
2023-11-30 15:47:20 +0900

    Divide controllers

--- src/main/java/com/goldenticket/controller/MainController.java
+++ src/main/java/com/goldenticket/controller/MainController.java
@@ -6,9 +6,9 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.goldenticket.DTO.Movie;
@@ -19,13 +19,13 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "메인 페이지", description = "메인페이지와 관련된 메서드들 모음")
-@RestController
+@Controller
 public class MainController {
 
 	@Autowired
 	private MovieMapper movieMapper;
 	
-	@Operation(summary = "메인 페이지 가져오기", description = "세션에 저장된 로그인 정보를 확인 후 그에 맞는 메인 페이지를 가져옵니다.")
+	// 세션에 저장된 로그인 정보를 확인 후 그에 맞는 메인 페이지를 가져옵니다.
 	@GetMapping("")
 	public ModelAndView getAll(@Parameter(description = "세션(로그인정보)") HttpSession session){
 
@@ -63,7 +63,7 @@ public class MainController {
 		}
 	}
 	
-	@Operation(summary = "오늘의 영화 갱신", description = "@Scheduled 애너테이션을 이용하여 매일 자정에 오늘의 영화 테이블의 영화들을 갱신합니다.")
+	// @Scheduled 애너테이션을 이용하여 매일 자정에 오늘의 영화 테이블의 영화들을 갱신합니다.
     @Scheduled(cron = "0 0 0 * * ?") // 매일 자정에 실행되어 오늘의 영화 테이블을 갱신한다.
 	@PostMapping("/todaymovie")
 	public int saveNewtodaymovie() {
